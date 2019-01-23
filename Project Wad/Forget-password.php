c641cd12d00445df2aba7444a133e17fdbd09d07
M.Asjad Farooq <40624415+l1f16bscs0389@users.noreply.github.com>
2019-01-23 10:29:47 +0500

    Template and convert all file to php

---
+++ Project Wad/Forget-password.php
@@ -0,0 +1,44 @@
+<?php
+$title = "Acme Web Design | Forget-Password ";
+$content = '
+
+<section id="main">
+    <div class="container">
+
+
+        <aside id="sidebar">
+            <div class="dark">
+
+                <p>Email:</p>
+                <i class="fas fa-user-circle mt-1"></i> <input type="email" placeholder="Enter Email... ">
+
+                <p>Password:</p>
+                <i class="fas fa-lock mt-1"></i> <input type="Password" placeholder="Enter Password... ">
+
+                <p>Confirm Password:</p>
+                <i class="fas fa-lock mt-1"></i> <input type="Password" placeholder="Enter Password Again ... ">
+
+                <p><br></p>
+
+                <button type="submit" class="button_4">Forget-Password</button>
+
+                </form>
+
+            </div>
+        </aside>
+
+    </div>
+
+</section>
+
+
+
+
+<footer>
+  <p>Copy Rights 2018-2019</p>
+</footer>
+
+
+';
+include 'template.php';
+ ?>
