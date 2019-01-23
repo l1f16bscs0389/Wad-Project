4c1d3c173f8256ea3dfa9613911743e0faf548d4
ahmedyounas <mahmedyounas@gmail.com>
2019-01-23 19:55:00 +0500

    resolve issues in admin login and admin panel

--- Project Wad/Admin Panel/admin_panel.php
+++ Project Wad/Admin Panel/admin_panel.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION['user_email'])){
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <title>Acme Web Design | Admin Panel </title>
     <link rel="stylesheet" href="..\css\style.css">
-    <link rel="stylesheet" type="text/css" href="css/custom.css">
+
 </head>
 
 <body>
